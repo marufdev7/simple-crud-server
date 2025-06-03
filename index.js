4b2b4295e4d8027a0059da8ba82fbad10a1eb0e8
mrfhmd7 <mrf.hmd7@gmail.com>
2025-06-03 16:05:34 +0600

    get a single user

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log('user', user);
